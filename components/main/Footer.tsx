52920f662167cb1498b060d902accc860d85a19a
Deep Pal <112393265+deepcs2004@users.noreply.github.com>
2023-12-19 13:32:14 +0530

    updated

--- components/main/Footer.tsx
+++ components/main/Footer.tsx
@@ -7,9 +7,13 @@ import {
     RxLinkedinLogo,
 } from "react-icons/rx";
 
+import Link from "next/link";
+
 import { FaYoutube } from "react-icons/fa";
 
 const Footer = () => {
+
+    
     return (
         <div className="w-full h-full bg-transparent text-gray-200 shadow-lg p-[15px] ">
             <div className="w-full flex flex-col items-center justify-center m-auto">
@@ -58,8 +62,15 @@ const Footer = () => {
                                 </a>
                             </p>
                         </div>
+
+
+
+
                     </div>
                 </div>
+
+                
+
 
                 <div className="mb-[20px] text-[15px] text-center text-bold">
                     &copy; MoneyView 2023 Inc. All rights reserved
